f8f7ad1fe9f4d4c987ea5a1f719c73cc8358306c
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-21 21:51:15 +0530

    Adding file to UC1 Branch

---
+++ src/com/SnakeLadder_PP/Position_Of_Player.java
@@ -0,0 +1,11 @@
+package com.SnakeLadder_PP;
+
+public class Position_Of_Player 
+{
+	public static void main(String[] args) 
+	{
+		int position = 0;
+		System.out.println("The Position of the Player is: "+position);
+	}
+
+}
