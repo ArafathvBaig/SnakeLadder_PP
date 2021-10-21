3c9ecdde6bd117ccfb6e3af011522248270a9ffb
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-21 22:40:20 +0530

    Adding the File to UC2 Branch

---
+++ src/com/SnakeLadder_PP/RollTheDie.java
@@ -0,0 +1,11 @@
+package com.SnakeLadder_PP;
+
+public class RollTheDie 
+{
+	public static void main(String[] args) 
+	{
+		int dieCheck = ((int)(Math.random()*10)%6)+1;
+		System.out.println("The number on die is: "+dieCheck);
+
+	}
+}
